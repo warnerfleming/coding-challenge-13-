3a36572650faba191c0338714fcea7bf78724d7e
warnerfleming <fleming14@usf.edu>
2024-11-03 14:28:38 -0500

    Fetch product data and display basic information

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+//Task 2
 
+document.addEventListener(`DOMContentLoaded`, ()=>{
+    const productContainer = document.getElementById(`productContainer`)
+    const error = document.getElementById(`error`)
+
+    fetch(`https://www.course-api.com/javascript-store-products`)
+    .then(response => {
+     if (!response.ok){
+        throw new Error(`No response`)
+     }
+     return response.json()
+    })
+    .then(info =>{
+        displayProducts(info)
+    })
+
+    .catch(errorOccur => {
+        error.textcontent = `Error: Failure to display products`
+
+    })
+})
